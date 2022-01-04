d665bdefafa33e0526a6bdfc75daa8a7def9e3b9
LucasGreard <86890031+LucasGreard@users.noreply.github.com>
2022-01-04 17:41:28 +0100

    Authentification non fini, erreur /connect/google

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    KnpU\OAuth2ClientBundle\KnpUOAuth2ClientBundle::class => ['all' => true]
 ];
