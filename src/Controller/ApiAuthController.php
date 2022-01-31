fe16cf3ad15e147b11ffc98fb4cfe4a50ecb63eb
LucasGreard <86890031+LucasGreard@users.noreply.github.com>
2022-01-31 19:43:36 +0100

    Ajout d'une authentification en POST

--- src/Controller/ApiAuthController.php
+++ src/Controller/ApiAuthController.php
@@ -7,6 +7,7 @@ use App\Exception\ResourceValidationException;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\Post;
 use FOS\RestBundle\Controller\Annotations\View;
 use OpenApi\Annotations as OA;
 use Firebase\JWT\JWT;
@@ -38,6 +39,59 @@ class ApiAuthController extends AbstractController
             $userEmail = $request->headers->get("php-auth-user");
             $pw = $request->headers->get("php-auth-pw");
             if (!$userEmail || !$pw)
+                throw new ResourceValidationException("Username or Password are blank");
+            $user = $doctrine->getRepository(Client::class)->findClientByUsernameAndApikey($userEmail, $pw);
+            if (!$user)
+                throw new ResourceValidationException("Client is invalid or does not exist");
+
+            return $this->json([
+                "Token" =>  $this->createJWT($user[0]['email']),
+                "Expiration time (in seconds)" => 3600,
+                "Type" => "Bearer",
+                "Email" => $user[0]['email']
+            ], 200);
+        } catch (ResourceValidationException $e) {
+            return $this->json([
+                "error" => $e->getMessage()
+            ], 400);
+        }
+    }
+
+    /**
+     * @Post(
+     *     path = "v1/api/auth",
+     *     name = "api_auth_post"
+     * )
+     * @View(statusCode=200)
+     * 
+     * @OA\Response(
+     *     response=200,
+     *     description="Returns a api token to connect (Need your API KEY and your email) In the body"
+     *     )
+     * )
+     * @OA\Parameter(
+     *     name="Authentification",
+     *     in="query",
+     *     description="use like this in body : { 'email' : 'monemail@gmail.com', 'api_key' : 'myApiKey' } ",
+     *     @OA\Schema(type="json")
+     * )
+     * @OA\Response(
+     *     response=400,
+     *     description="Client is invalid or does not exist or Username or Password are blank"
+     *     )
+     * )
+     * @param $jsonAuth
+     */
+    public function apiAuthPost(ManagerRegistry $doctrine, Request $request)
+    {
+        try {
+            $data = json_decode($request->getContent(), true);
+            if (!$data || $data = null)
+                throw new ResourceValidationException("JSON missing, read documentation at /v1/api/doc");
+
+            $userEmail = $data['email'];
+            $pw = $data['api_key'];
+            if (!$userEmail || !$pw || !$data)
                 throw new ResourceValidationException("Username or Password are blank");
             $user = $doctrine->getRepository(Client::class)->findClientByUsernameAndApikey($userEmail, $pw);
             if (!$user)
